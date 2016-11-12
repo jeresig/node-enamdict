a95a2b85be3f07c2ecdbe2eb3bbe8108beda56e6
John Resig <jeresig@gmail.com>
2016-11-12 13:56:16 -0500

    Fix Enamdict downloader and update dict file.

--- download-enamdict.js
+++ download-enamdict.js
@@ -1,6 +1,6 @@
 /**
  * Utility for downloading the latest ENAMDICT, converting it to the right
- * encoding, re-formatting it, and saving as a compressed file.
+ * encoding, re-formatting it, and outputting it to STDOUT.
  */
 var enamdict = require("./enamdict");
 var fs = require("fs");
@@ -12,15 +12,6 @@ var Iconv = require("iconv").Iconv;
 // The default URL where the enamdict file is located
 var enamdictURL = "http://ftp.monash.edu.au/pub/nihongo/enamdict.gz";
 
-try {
-    fs.statSync(enamdict.enamdictFile);
-    process.exit(0);
-} catch(e) {
-    // If it doesn't exist then we need to download the dict
-}
-
-console.log("Downloading ENAMDICT...");
-
 // Download the ENMADICT database
 var stream = request(enamdictURL)
     // Extract it...
@@ -30,28 +21,8 @@ var stream = request(enamdictURL)
 
 // Load it into the enamdict module
 enamdict.init(stream, function() {
-    console.log("Processing...");
-
     // And then convert it to the desired file format
     enamdict.process(function(err, data) {
-        console.log("Outputting...");
-
-        // Write out the result to a file!
-        // Stick the string into the stream.
-        var stream = new Stream();
-
-        stream.pipe = function(dest) {
-            dest.write(data);
-            return dest;
-        };
-
-        stream
-            // Make sure it's gzipped
-            .pipe(zlib.createGzip())
-            // Write it out to a file
-            .pipe(fs.createWriteStream(enamdict.enamdictFile))
-            .on("end", function() {
-                console.log("DONE");
-            });
+        return console.log(data);
     });
 });
